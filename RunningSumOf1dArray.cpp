ded9509ee323ea9ffb4d79ae637ce2f42d446339
yyashh <76594093+yyashh@users.noreply.github.com>
2021-06-25 22:14:30 +0530

    Add files via upload

---
+++ RunningSumOf1dArray.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int i;
+        for(i=1;i<nums.size();i++){
+            nums[i]=nums[i]+nums[i-1];
+        }
+       return nums;
+    }
+};
