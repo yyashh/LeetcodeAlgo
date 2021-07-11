8c61432dd59000f25b1a8572c7501a79df270e5d
yyashh <76594093+yyashh@users.noreply.github.com>
2021-07-12 02:10:52 +0530

    Add files via upload

---
+++ SortArrayByParity.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        vector<int> a;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2!=0){
+                a.push_back(nums[i]);
+            }
+            else{
+                a.insert(a.begin(),nums[i]);
+            }
+        }
+        return a;
+    }
+};
