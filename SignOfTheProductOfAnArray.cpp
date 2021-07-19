5e35e6219f5bdf58e4cbfecdd96a9164e47c2e8e
yyashh <76594093+yyashh@users.noreply.github.com>
2021-07-19 19:45:37 +0530

    Add files via upload

---
+++ SignOfTheProductOfAnArray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int res=1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0)
+                return 0;
+            else if(nums[i]<0)
+                res=-res;   
+        }
+        return res;
+    }
+};
